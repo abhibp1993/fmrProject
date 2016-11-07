c809858554e74cfbe9b86af57292d394a98ea7e6
espadandy <espadandy@gmail.com>
2016-11-06 19:13:37 -0500

    Add the world to the input

--- setRoad.m
+++ setRoad.m
@@ -1,24 +1,21 @@
 % 6 Nov 2016 by Andy
-% setRoad(s,e) is a function to generate a road between start point s and
-% end point e.
-% The output is a cell containing the road.
+% setRoad(w, s,e) is a function to generate a road between start point s and
+% end point e, in World w.
+% The output is the world containing the road.
 
-function WholeRoad = setRoad(s, e)
-
+function w = setRoad(w, s, e)
 if (s(1) == e(1))
-	WholeRoad = cell(1,abs(s(2)-e(2) ) );
 	id = 1;
 	for i = s(2):e(2)
 		r = Road(id, s(1),i);
-		WholeRoad{id} = r;
+		w = w.addObject(r);
 		id = id+1;
 	end
 elseif(s(2) == e(2))
-	WholeRoad = cell(1,abs(s(1)-e(1) ) );
 	id = 1;
 	for i = s(1):e(1)
 		r = Road(id, s(2),i);
-		WholeRoad{id} = r;
+		w = w.addObject(r);
 		id = id+1;
 	end
 else
