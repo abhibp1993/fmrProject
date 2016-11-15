e6b838f1635b672dfee0ac5004a27f8abee35e1b
abhibp1993 <abhi.bp1993@gmail.com>
2016-11-14 22:00:19 -0500

    added grid, graph with A*

---
+++ grid.py
@@ -0,0 +1,64 @@
+"""
+Author: Abhishek Kulkarni
+Modified: November 14, 2016
+
+Implements basic data structures world creation
+"""
+
+
+class Grid(object):
+    GRASS = 'grass'
+    ROAD = 'road'
+    STOP = 'stop'
+    OCC = 'occupied'
+
+    def __init__(self, dim=20):
+        """
+        Creates a dim x dim sized box world with default grass everywhere.
+
+        @param dim: size of the discrete world.
+        """
+        self.dim = dim
+        self.AP = {Grid.GRASS: list(), Grid.ROAD: list(), Grid.STOP: list(), Grid.OCC: list()}
+
+        for i in range(dim):
+            for j in range(dim):
+                self.AP[Grid.GRASS].append([i, j])
+
+    def addRoad(self, cells):
+        """
+         Sets the given cells as road. The road doesn't need to be straight.
+         Any turns, will be taken care of in planning algorithm.
+        """
+        for c in cells:
+            try:
+                self.AP[Grid.GRASS].remove(c)
+            except:
+                pass
+
+        for c in cells:
+            try: self.AP[Grid.ROAD].remove(c)
+            except: pass
+            finally: self.AP[Grid.ROAD].append(c)
+
+    def addStop(self, cell):
+        """
+        Adds a stop sign to cell.
+        Stop sign can only be added if the cell is a road.
+
+        @param cell: valid (i, j)
+        """
+        if cell in self.AP[Grid.ROAD]:
+            self.AP[Grid.STOP].append(cell)
+
+    def __str__(self):
+        return str(self.AP)
+
+# Basic Testing
+if __name__ == '__main__':
+    g = Grid(4)
+    g.addRoad([[1, i] for i in range(3)])
+    g.addStop([1, 0])
+    g.addStop([3, 1])
+
+    print g
