be0cfe5af521df01683665881f02959ef7cbc68f
abhibp1993 <abhi.bp1993@gmail.com>
2016-12-25 12:24:12 -0500

    implemented, tested Router._graphify.

--- simcar.py
+++ simcar.py
@@ -8,6 +8,9 @@ Defines car with discrete dynamics for simulation.
 
 # Imports
 import sm
+import networkx as nx
+import numpy as np
+import simworld
 
 
 # Define macros
@@ -15,6 +18,8 @@ NORTH = 90
 WEST = 180
 SOUTH = 270
 EAST = 0
+
+
 
 
 # TODO: Make the operations efficient. There's a lot of scope for improvement!
@@ -247,7 +252,133 @@ class AvoidObstacle(sm.SM):
 
 
 class Router(sm.SM):
-    def __init__(self):
+    def __init__(self, world, aggression):
+        """
+        Constructs a Router state machine.
+        :param world: simworld.World object
+        :param aggression: dict of cost structure:
+            {'r2r': float,  # road2road
+             'r2g': float,  # road2grass
+             'r2o': float,  # road2obstacle
+             'g2r': float,  # grass2road
+             'g2g': float,  # grass2grass
+             'g2o': float,  # grass2obstacle
+            }
+
+        These costs define the aggression of car. For example,
+        if cost of road2grass = 20 and road2road = 10, then car will
+        prefer to go over grass if length of road is 2 cells more than
+        that of driving on grass.
+
+        In essence, the difference between the costs is as important as
+        the costs themselves.
+        """
+        # Internal variables
+        self.grf = self._graphify(world, aggression)
+
+    def updateAggression(self, world, aggValue):
+        """
+        The function may be introduced later to update weights of
+        internal graph.
+
+        :param aggValue: dictionary as mentioned in description of constructor.
+        :return: None
+        """
+        raise NotImplementedError('Router.updateAggression: Is not implemented in this version of project.')
+
+    def getNextValues(self, state, inp):
+        """
+        Transition Function.
+
+        :param state: 2-tuple (goal <integer>, last computed path <list>)
+        :param inp: 2-tuple (goal <integer>, car pose)
+        :return: suggested next cell label <integer>
+        """
         pass
 
+    def _graphify(self, world, aggression):
+        """
+        Generates graph representation of the world, and defines
+        cost of edges based on aggression.
 
+        :param world: simworld.World object.
+        :param aggression: dictionary as described in constructor of Router class.
+        :return:
+        """
+        assert isinstance(world, simworld.World), 'Router._graphify: world must be simworld.World object.'
+
+        # Initialize graph
+        grf = nx.DiGraph()
+
+        # Get dimensions of world
+        dim = world.dim
+
+        # For all cells in the world do
+        for i in range(dim[0]):
+            for j in range(dim[1]):
+                # Get the valid neighbors of the cell (Can be made efficient later!)
+                neighbors = list()
+                if (i, j) in world: neighbors.append(world.label((i, j)))
+                if (i, j+1) in world: neighbors.append(world.label((i, j+1)))
+                if (i, j-1) in world: neighbors.append(world.label((i, j-1)))
+                if (i+1, j) in world: neighbors.append(world.label((i+1, j)))
+                if (i+1, j-1) in world: neighbors.append(world.label((i+1, j-1)))
+                if (i+1, j+1) in world: neighbors.append(world.label((i+1, j+1)))
+                if (i-1, j) in world: neighbors.append(world.label((i-1, j)))
+                if (i-1, j-1) in world: neighbors.append(world.label((i-1, j-1)))
+                if (i-1, j+1) in world: neighbors.append(world.label((i-1, j+1)))
+
+
+                # For each neighbor, find the cost of edge
+                fromLabel = world.label((i, j))
+                for toLabel in neighbors:
+                    fromAP = world.ap(fromLabel)
+                    toAP = world.ap(toLabel)
+
+                    # Enumerate Exhaustively to check which case we have
+                    if fromAP.isRoad and toAP.isRoad:
+                        cost = aggression['r2r']
+
+                    elif fromAP.isRoad and not toAP.isRoad and not toAP.isStatObs:
+                        cost = aggression['r2g']
+
+                    elif fromAP.isRoad and toAP[simworld.STAT_OBS]:
+                        cost = aggression['r2o']
+
+                    elif not fromAP.isRoad and not fromAP.isStatObs and toAP.isRoad:
+                        cost = aggression['g2r']
+
+                    elif not fromAP.isRoad and not fromAP.isStatObs and not toAP.isRoad and \
+                            not toAP.isStatObs:
+                        cost = aggression['g2g']
+
+                    elif not fromAP.isRoad and not fromAP.isStatObs and toAP.isStatObs:
+                        cost = aggression['g2o']
+
+                    else:
+                        print('Router.graphify: Something weird is happening!')
+                        cost = float('Inf')
+
+                    # Add nodes
+                    grf.add_edge(fromLabel, toLabel, weight=cost)
+
+        return grf
+
+
+if __name__ == '__main__':
+
+    labels = {'isRoad': np.ones((3, 3)), 'isStatObs': np.ones((3, 3)), 'stopSign': np.zeros((3, 3))}
+    myWorld = simworld.World(labels)
+
+    agg = {
+        'r2r': 50,
+        'r2g': 100,
+        'r2o': float('Inf'),
+        'g2r': 0,
+        'g2g': 100,
+        'g2o': float('Inf'),
+    }
+    r = Router(myWorld, agg)
+    print(r.grf.number_of_nodes(), r.grf.number_of_edges())
+    for edg in r.grf.edges():
+        print (edg)
